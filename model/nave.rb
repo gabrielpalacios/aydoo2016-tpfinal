d425b20cc2109866183a90f0ff1ace2d09d5f540
palaciosgabriel@hotmail.com <palaciosgabriel@hotmail.com>
2016-06-25 20:11:24 -0300

    una nave con test de vida y masa usando const.

--- model/nave.rb
+++ model/nave.rb
@@ -1,10 +1,22 @@
 class Nave
 
+
   attr_accessor :masa, :vida
 
 
   def initialize()
-
+    @masa = 100
+    @vida = 100
 
   end
+
+  def calcularSiEstaVivo
+
+    tieneVidaMasa = true
+    if (@masa <= 0 || @vida <= 0)
+      tieneVidaMasa = false
+    end
+    return tieneVidaMasa
+  end
+
 end
--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -3,14 +3,28 @@ require_relative '../model/nave'
 
 describe 'Nave' do
 
-  it 'setear con set vida y masa a una nave y releer sus valores' do
+  it 'setear vida y masa 100 a una nave y releer sus valores' do
     objeto = Nave.new
     objeto.vida = 100
     objeto.masa = 100
-    vectorEsperadoVidaYMasa = 100
-    expect(objeto.vida).to eq vectorEsperadoVidaYMasa
-    expect(objeto.masa).to eq vectorEsperadoVidaYMasa
+    valorEsperadoVidaYMasa = 100
+    expect(objeto.vida).to eq valorEsperadoVidaYMasa
+    expect(objeto.masa).to eq valorEsperadoVidaYMasa
   end
+
+  it 'setear vida y masa 100 a una nave y releer sus valores usando constructor' do
+    objeto = Nave.new
+    valorEsperadoVidaYMasa = 100
+    expect(objeto.vida).to eq valorEsperadoVidaYMasa
+    expect(objeto.masa).to eq valorEsperadoVidaYMasa
+  end
+
+  # it 'setear vida 0 a una nave y releer sus valores para saber q esta muerta' do
+  #   objeto = Nave.new
+  #   objeto.vida = 0
+  #   valorEsperadoVidaYMasa = false
+  #   expect(objeto.calcularSiEstaVivo).to eq valorEsperadoVidaYMasa
+  # end
 
   end
 
